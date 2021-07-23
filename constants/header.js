0bff44c442b80543f47a8be151a80426668f75e0
Ejaz Ali <ejazali4@protonmail.ch>
2021-07-23 10:33:23 +0100

    💄

--- constants/header.js
+++ constants/header.js
@@ -1,12 +1,16 @@
 return `
 <div class="header">
     <div class="header-inner">
-        <div></div>
+        <div style="width: 50px; justify-self: left;">
+            <img src="/flse/images/flselogo.svg" onclick="window.location.href = '/flse'" style="height: calc(100% - 30px); align-self: center; display: inline-block; cursor: pointer;"/>
+        </div>
         <div class="header-midpoint">
             <a>Releases</a>
             <a>Switching</a>
         </div>
-        <div></div>
+        <div>
+            <a>GitHub</a>
+        </div>
     </div>
 </div>
 `;
